b5dcbc107dacb30bc5624f2a6f174b13ca1be008
unknown <beastcchauhan@gmail.com>
2026-02-16 21:56:37 +0530

    asf

--- src/com/GLA/Inheritance/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.GLA.Inheritance;
-
-public class Animal {
-    public void walks(String[] args){
-        System.out.println("walk on four legs");
-    }
-}
--- src/com/GLA/Inheritance/Dogs.java
+++
@@ -1,7 +0,0 @@
-package com.GLA.Inheritance;
-
-public class Dogs extends Animal {
-    public void sound(String[] args) {
-        System.out.println("barks");
-    }
-}
