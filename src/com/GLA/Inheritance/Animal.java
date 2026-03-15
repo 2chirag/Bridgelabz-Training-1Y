94fc0ea9440fc4341780ea2f3128037b66f89663
unknown <beastcchauhan@gmail.com>
2026-03-16 02:25:21 +0530

    sdf

--- src/com/GLA/Inheritance/Animal.java
+++ src/com/GLA/Inheritance/Animal.java
@@ -1,7 +1,8 @@
 package com.GLA.Inheritance;
 
 public class Animal {
-    public void walks(String[] args){
+    public void walks(String[] args) {
+
         System.out.println("walk on four legs");
     }
 }
