17ab19630dc1fedffef97164fd45574219219d04
unknown <beastcchauhan@gmail.com>
2026-03-17 11:39:47 +0530

    afsdg

---
+++ src/LeetCode2119.java
@@ -0,0 +1,16 @@
+public class LeetCode2119 {
+        public boolean isSameAfterReversals(int num) {
+            if(num!=0){
+                if(num%10==0){
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+            else {
+                return true;
+            }
+        }
+    }
+}
