16cb807c10d59398bc516fd3f19c5112fd53466b
unknown <beastcchauhan@gmail.com>
2026-03-23 15:50:18 +0530

    asfsdg

---
+++ src/com/GLA/Maruti/practice.java
@@ -0,0 +1,18 @@
+class Vehicle {
+    void display(){
+        System.out.println("vehicle class");
+    }
+}
+class Maruti extends Vehicle {
+    void speed(){
+        System.out.println("car run fast");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Vehicle obj = new Maruti();
+        Maruti m = (Maruti) obj;
+        obj.display();
+        m.speed();
+    }
+}
