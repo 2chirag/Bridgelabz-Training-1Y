f56c694c18c12f8edc4db251f61004ba7668c5a2
unknown <beastcchauhan@gmail.com>
2026-04-10 15:37:37 +0530

    dsgsdg

--- src/com/GLA/cllections/Random.java
+++ src/com/GLA/cllections/Random.java
@@ -1,4 +1,29 @@
 package com.GLA.cllections;
 
 public class Random {
+    public static void main(String[] args){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(45);
+        set.add(50);
+        set.add(18);
+        set.add(18);
+        set.add(15);
+        set.add(9);
+
+        System.out.println(set);
+        set.remove(15);
+        System.out.println(set);
+        System.out.println(set.contains);
+        System.out.println(set.isEmpty());
+        System.out.println(set.size());
+
+        for(int val :set){
+            System.out.println(val+" ");
+        }
+        Iterator it = set.Iterator();
+        while(it.hasNaxt()){
+            System.out.println(it.next());
+        }
+
+    }
 }
